2f6349ff927197a7c530ee1746dba3ed8e8d1080
Trong Tri Bui <buitrongtri2004@gmail.com>
2024-10-10 22:44:12 +0700

    edit batch koi routes

--- src/routes/batchRoutes.js
+++ src/routes/batchRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Định nghĩa các route
 router.get('/get', batchKoiController.getAllBatchKoiFish);
-router.get('/get/:id', batchKoiController.getBatchKoiFishById);
+router.get('/:id', batchKoiController.getBatchKoiFishById);
 router.post('/create', batchKoiController.createBatchKoiFish);
 router.put('/update/:id',batchKoiController.updateBatchKoiFish);
 router.delete('/delete/:id', batchKoiController.deleteBatchKoiFish)
